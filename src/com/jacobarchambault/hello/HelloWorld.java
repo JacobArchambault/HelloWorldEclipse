84cd3851ed2cbbcbf6af2a79e36ebceecfa64e42
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-08 16:16:23 -0500

    Add change

--- src/com/jacobarchambault/hello/HelloWorld.java
+++ src/com/jacobarchambault/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jacobarchambault.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git change!");
 	}
 
 }
