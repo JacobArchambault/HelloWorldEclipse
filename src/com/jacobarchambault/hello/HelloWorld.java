003f2b6d20896e3a9baa263c18b89daf2ab1c268
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-09 12:47:45 -0500

    SSH Push from Eclipse

--- src/com/jacobarchambault/hello/HelloWorld.java
+++ src/com/jacobarchambault/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jacobarchambault.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git change with SSH key!");
+		System.out.println("Hello Git change with SSH key from Eclipse!");
 	}
 	
 }
