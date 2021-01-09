f4f26a8c49b53cea8e3f84a464c2e821a32b9453
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-08 22:10:06 -0500

    Second try with Personal Access Token

--- src/com/jacobarchambault/hello/HelloWorld.java
+++ src/com/jacobarchambault/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jacobarchambault.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git change with Personal access token!");
+		System.out.println("Hello second Git change with Personal access token!");
 	}
 	
 }
