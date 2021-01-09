4bfb589cfa7227f5c9d30c08f6421d3dfaa7713c
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-09 12:46:11 -0500

    SSH Key push attempt

--- src/com/jacobarchambault/hello/HelloWorld.java
+++ src/com/jacobarchambault/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.jacobarchambault.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git change with Personal access token from command line!");
+		System.out.println("Hello Git change with SSH key!");
 	}
 	
 }
